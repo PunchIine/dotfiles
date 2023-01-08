444d0c53f02193633edddba1fb5b829f0ae8a4c5
LazyOnion <811735602@qq.com>
2023-01-08 12:35:59 +0800

    update

---
+++ nvim/lua/user/im-select.lua
@@ -0,0 +1,4 @@
+"vim.autoSwitchInputMethod.enable": true,
+"vim.autoSwitchInputMethod.defaultIM": "1",
+"vim.autoSwitchInputMethod.obtainIMCmd": "/usr/bin/fcitx-remote",
+"vim.autoSwitchInputMethod.switchIMCmd": "/usr/bin/fcitx-remote -t {im}",
